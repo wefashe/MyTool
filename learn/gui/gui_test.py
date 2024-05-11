c253ca9ff160028a78e4b03bc0b6fa956e2acfb6
wefashe <wefashe@qq.com>
2024-05-11 13:35:23 +0800

    gui学习

---
+++ learn/gui/gui_test.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import ttk # tkinter扩展tkk
+from tkinter import filedialog # 选择文件
+from tkinter import messagebox # 消息弹框
+import asyncio # 异步编程
+import threading # 多线程
+
+win = tk.Tk() # 创建 Tkinter 窗口
+
+# Variable: BooleanVar, DoubleVar, IntVar, StringVar 可以进行双向绑定
+
+#显示标签
+def print_tk_text(text):
+   tk.Label(win, text=text, font=('Helvetica 13 bold')).pack()
+def print_ttk_text(text):
+   ttk.Label(win, text=text, font=('Helvetica 13 bold')).pack()
+
+button1 = tk.Button(win, text="点击tk按钮",  command=lambda:print_tk_text('tk按钮被点击了')) # 创建按钮
+button1.pack(pady=10) # 按钮添加到窗口
+button2 = ttk.Button(win, text="点击ttk按钮",  command=lambda:print_tk_text('ttk按钮被点击了')) # 创建按钮
+button2.pack(pady=10) # 按钮添加到窗口
+
+
+# TODO 
+
+
+win.geometry("600x250") # 设置窗口大小和初始位置
+
+win.wm_attributes("-topmost", 1)  # 窗口置顶
+win.iconbitmap('favicon.ico') # 设置图标
+win.title('测试窗口') # 标题
+win.mainloop() # 进入消息循环
