ecbc5f3ec68d8704a552c0ace8a782c126df4e01
wefashe <wefashe@qq.com>
2024-05-11 13:50:11 +0800

    学习

--- learn/gui/gui_test.py
+++ learn/gui/gui_test.py
@@ -11,13 +11,15 @@ win = tk.Tk() # 创建 Tkinter 窗口
 
 #显示标签
 def print_tk_text(text):
-   tk.Label(win, text=text, font=('Helvetica 13 bold')).pack()
+   file_path = filedialog.askopenfilename()
+   tk.Label(win, text=text + file_path, font=('Helvetica 13 bold')).pack()
 def print_ttk_text(text):
+   messagebox.showinfo("ttk按钮被点击了", "哈哈哈")
    ttk.Label(win, text=text, font=('Helvetica 13 bold')).pack()
 
-button1 = tk.Button(win, text="点击tk按钮",  command=lambda:print_tk_text('tk按钮被点击了')) # 创建按钮
+button1 = tk.Button(win, text="点击tk按钮",  command=lambda:print_tk_text('tk按钮被点击了,选择了文件：')) # 创建按钮
 button1.pack(pady=10) # 按钮添加到窗口
-button2 = ttk.Button(win, text="点击ttk按钮",  command=lambda:print_tk_text('ttk按钮被点击了')) # 创建按钮
+button2 = ttk.Button(win, text="点击ttk按钮",  command=lambda:print_ttk_text('ttk按钮被点击了')) # 创建按钮
 button2.pack(pady=10) # 按钮添加到窗口
 
 
@@ -25,8 +27,8 @@ button2.pack(pady=10) # 按钮添加到窗口
 
 
 win.geometry("600x250") # 设置窗口大小和初始位置
-
+win.configure(bg='IndianRed') # 背景色
 win.wm_attributes("-topmost", 1)  # 窗口置顶
-win.iconbitmap('favicon.ico') # 设置图标
+win.iconbitmap('learn/gui/favicon.ico') # 设置图标
 win.title('测试窗口') # 标题
 win.mainloop() # 进入消息循环
