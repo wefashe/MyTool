9f87242829a524f85c1ec60f52cb44782d363caf
wefashe <wefashe@qq.com>
2024-05-21 10:27:44 +0800

    完善

--- extract/steam_server.py
+++ extract/steam_server.py
@@ -26,64 +26,98 @@ cmList = resp_json['response']['serverlist']
 cmList = sorted(cmList, key=lambda x: x["wtd_load"])
 
 cmList = [server for server in cmList if server['type'] =='websockets' and server['realm'] == 'steamglobal']
+# print(json.dumps(cmList, ensure_ascii=False, indent=2))
 
-rand_upper_bound = min(20, len(cmList))
-cm = cmList[random.randint(0, rand_upper_bound - 1)]
-
-# print(json.dumps(serverlist, ensure_ascii=False, indent=2))
-
-def encode(proto, obje):
+def get_cm_server(): # 随机获取服务器信息
+    return cmList[random.randint(0, min(20, len(cmList)) - 1)]
+def encode(proto, obje): # 编码
     return json_format.Parse(json.dumps(obje), proto).SerializeToString()
-def decode(proto, msge):
+def decode(proto, msge): # 解码
     proto.ParseFromString(msge)
     return json_format.MessageToJson(proto)
+async def ping(ws): # 心跳检测
+    while True:
+        try:
+            await ws.send('ping')
+            await asyncio.sleep(10)
+        except:
+            break
 
-async def main(cm):
-    async with websockets.connect(f'wss://{cm["endpoint"]}/cmsocket/') as websocket:
-        encoded_client_Hello = encode( steammessages__clientserver__login__pb2.CMsgClientHello(), {
-            'protocol_version': 65580
-        })
+
+async def main():
+    while True: # 换服务器
+        cm = get_cm_server()
+        while True: # 断线重连
+            try:
+                print(f'Connecting to {cm["endpoint"]}')
+                async with websockets.connect(f'wss://{cm["endpoint"]}/cmsocket/') as websocket:
+                    encoded_client_Hello = encode( steammessages__clientserver__login__pb2.CMsgClientHello(), {
+                        'protocol_version': 65580
+                    })
+
+                    encoded_proto_buf_header = encode( steammessages__base__pb2.CMsgProtoBufHeader(), {
+                        'steamid': '0',
+                        'client_sessionid': 0
+                    })
+
+                    header = bytearray(8)
+                    PROTO_MASK = 0x80000000
+                    header[:4] = (9805 | PROTO_MASK).to_bytes(4, byteorder='little', signed=False)
+                    header[4:] = len(encoded_proto_buf_header).to_bytes(4, byteorder='little', signed=False)
+                    await websocket.send(header + encoded_proto_buf_header + encoded_client_Hello)
+
+                    asyncio.create_task(ping(websocket))
+
+                    timeout = 1000
+                    while True: 
+                        try:
+                            response = await asyncio.wait_for(websocket.recv(), timeout=timeout)            
+                        except websockets.exceptions.ConnectionClosed as e:
+                            # 1000 正常关闭码 1006 服务端内部错误异常关闭码
+                            print(e)
+                            if e.code == 1006:
+                                print('restart')
+                                await asyncio.sleep(2)
+                                break
+                        except asyncio.TimeoutError:
+                            print(f'Connecting to {cm["endpoint"]} timed out after {timeout} ms')
+                            timeout = min(1000, timeout * 2)
+                        else:
+                            if type(response) == 'bytes':
+                                print(f'Received unexpected frame type from {cm["endpoint"]}: {type(response)}')
+                            print(f"Received: {response}")
+                            rawEmsg = int.from_bytes(response[0:4], byteorder='little')
+                            hdrLength = int.from_bytes(response[4:8], byteorder='little')
+                            hdrBuf = response[8:8+hdrLength]
+                            msgBody = response[8+hdrLength:]
+
+                            if not rawEmsg & PROTO_MASK:
+                                raise ValueError(f"Received unexpected non-protobuf message {rawEmsg}")
+                            
+                            result = decode( steammessages__base__pb2.CMsgProtoBufHeader(), hdrBuf)
+
+                            print('decoded: ', result)
+                            eMsg = (rawEmsg & ~PROTO_MASK)  
+                            if eMsg != 1:
+                                print(f'11111111111')
+                            print('eMsg', eMsg, 'cm', cm["endpoint"])
+                            if eMsg == 751: # 751时 换服务器
+                                result = decode( steammessages__clientserver__login__pb2.CMsgClientLogonResponse(), msgBody)
+                                print(result)
+                                break
 
 
-        # client_Hello = steammessages__clientserver__login__pb2.CMsgClientHello()
-        # encoded_client_Hello = json_format.Parse(json.dumps({
-        #     'protocol_version': 65580
-        # }), client_Hello).SerializeToString()
+                            # TODO  具体业务
 
-        encoded_proto_buf_header = encode( steammessages__base__pb2.CMsgProtoBufHeader(), {
-            'steamid': '0',
-            'client_sessionid': 0
-        })
+                            return # 结束方法
+            except ConnectionRefusedError as e:
+                print(e)
+                global count
+                # 重连3次
+                if count == 3:  
+                    return
+                count += 1
+                await asyncio.sleep(2)
 
-        header = bytearray(8)
-        PROTO_MASK = 0x80000000
-        header[:4] = (9805 | PROTO_MASK).to_bytes(4, byteorder='little', signed=False)
-        header[4:] = len(encoded_proto_buf_header).to_bytes(4, byteorder='little', signed=False)
-        await websocket.send(header + encoded_proto_buf_header + encoded_client_Hello)
-
-        response = await websocket.recv()
-        if type(response) == 'bytes':
-            print(f'Received unexpected frame type from {cm["endpoint"]}: {type(response)}')
-        print(f"Received: {response}")
-        rawEmsg = int.from_bytes(response[0:4], byteorder='little')
-        hdrLength = int.from_bytes(response[4:8], byteorder='little')
-        hdrBuf = response[8:8+hdrLength]
-        msgBody = response[8+hdrLength:]
-
-        if not rawEmsg & PROTO_MASK:
-            raise ValueError(f"Received unexpected non-protobuf message {rawEmsg}")
-        
-
-        result = decode( steammessages__base__pb2.CMsgProtoBufHeader(), hdrBuf)
-
-        print('decoded: ', result)
-        eMsg = (rawEmsg & ~PROTO_MASK)  
-        if eMsg != 1:
-            print(f'11111111111')
-        print('eMsg', eMsg, 'cm', cm["endpoint"])
-        if eMsg == 751:
-            result = decode( steammessages__clientserver__login__pb2.CMsgClientLogonResponse(), msgBody)
-            print(result)
-
-asyncio.get_event_loop().run_until_complete(main(cm))
+asyncio.get_event_loop().run_until_complete(main())
 
