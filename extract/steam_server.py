123c5fbbf9ded318934ecece80c3aaa0040701a0
wefashe <wefashe@qq.com>
2024-05-21 15:01:27 +0800

    完善

--- extract/steam_server.py
+++ extract/steam_server.py
@@ -9,9 +9,12 @@ import steammessages_clientserver_login_pb2 as steammessages__clientserver__logi
 import steammessages_base_pb2 as steammessages__base__pb2
 
 def get_cm_server(): # 随机获取服务器信息
+    global cmList 
     if 'cmList' not in locals() or len(cmList) == 0:
         # 获取 CM (Connection Manager) 服务器列表 用于 Steam 帐户登录认证、好友在线状态、聊天和游戏邀请等等方面 
         # cellid 地区相关id， format 格式, vdf js xml
+        # https://api.steampowered.com/ISteamDirectory/GetCMList/v0001/?cellid=0&format=js
+        # https://api.steampowered.com/ISteamDirectory/GetCMList/v1/?cellid=148&steamrealm=steamchina
         url = 'https://api.steampowered.com/ISteamDirectory/GetCMListForConnect/v0001/?cellid=0&format=js'
         headers = {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
@@ -29,28 +32,40 @@ def get_cm_server(): # 随机获取服务器信息
             'Upgrade-Insecure-Requests': '1',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
         }
-        resp = requests.get(url, headers=headers)
-        resp_json = resp.json()
-        if not resp_json['response'] or not resp_json['response']['success'] or not resp_json['response']['serverlist']:
-            return {}
-        cmList = resp_json['response']['serverlist']
-        # endpoint：一个必需的字符串属性，用于指定服务器的端点。
-        # legacy_endpoint：一个可选的字符串属性，用于指定旧版本服务器的端点。
-        # type：一个必需的字符串属性，用于指定服务器类型。
-        # dc：一个可选的字符串属性，用于指定数据中心。
-        # realm：一个必需的字符串属性，用于指定服务器所属的领域。
-        # load：一个可选的字符串属性，用于指定服务器的负载情况。
-        # wtd_load：一个可选的字符串属性，可能用于指定服务器的另一种负载情况。
-        cmList = [server for server in cmList if server['type'] =='websockets' and server['realm'] == 'steamglobal']
-        cmList = sorted(cmList, key=lambda x: x["wtd_load"])
-        # print(json.dumps(cmList, ensure_ascii=False, indent=2))
+        try:
+            resp = requests.get(url, headers=headers)
+            resp.raise_for_status()
+            resp_json = resp.json()
+            if not resp_json['response'] or not resp_json['response']['success'] or not resp_json['response']['serverlist']:
+                return {}
+            cmList = resp_json['response']['serverlist']
+            # endpoint：一个必需的字符串属性，用于指定服务器的端点。
+            # legacy_endpoint：一个可选的字符串属性，用于指定旧版本服务器的端点。
+            # type：一个必需的字符串属性，用于指定服务器类型。
+            # dc：一个可选的字符串属性，用于指定数据中心。
+            # realm：一个必需的字符串属性，用于指定服务器所属的领域。
+            # load：一个可选的字符串属性，用于指定服务器的负载情况。
+            # wtd_load：一个可选的字符串属性，可能用于指定服务器的另一种负载情况。
+            cmList = [server for server in cmList if server['type'] =='websockets' and server['realm'] == 'steamglobal']
+            cmList = sorted(cmList, key=lambda x: x["wtd_load"])
+            # print(json.dumps(cmList, ensure_ascii=False, indent=2))
+        except requests.exceptions.ConnectionError as e:
+            print('网络连接异常: ', e)
+        except requests.exceptions.Timeout as e:
+            print('连接超时: ', e)
+        except requests.exceptions.RequestException as e:
+            print('请求异常: ', e)
+        except requests.exceptions.HTTPError as e:
+            print(f'HTTP错误, 状态码: {e.response.status_code}, {e}')
+        except ValueError as e:
+            print('响应解析异常: ', e)
     return cmList[random.randint(0, min(20, len(cmList)) - 1)]
 
 def encode(proto, obje): # 编码
     return json_format.Parse(json.dumps(obje), proto).SerializeToString()
 def decode(proto, msge): # 解码
     proto.ParseFromString(msge)
-    return json_format.MessageToJson(proto)
+    return json.loads(json_format.MessageToJson(proto))
 
 async def ping(ws): # 心跳检测
     while True:
@@ -64,7 +79,7 @@ PROTO_MASK = 0x80000000
 PROTOCOL_VERSION = 65580
 
 Multi = 1
-CLIENT_HELLO = 9805,
+CLIENT_HELLO = 9805
 SERVICE_METHOD_CALL_FROM_CLIENT_NON_AUTHED = 9804
 CLIENT_LOG_ON_RESPONSE = 751
 
@@ -80,12 +95,13 @@ async def send_message(ws, msg_type, body):
         pass
     encoded_msg_proto_buf_header = encode(steammessages__base__pb2.CMsgProtoBufHeader(), msg_proto_buf_header)
     header = bytearray(8)
-    header[:4] = (int(msg_type) | PROTO_MASK).to_bytes(4, byteorder='little', signed=False)
+    header[:4] = (msg_type | PROTO_MASK).to_bytes(4, byteorder='little', signed=False)
     header[4:] = len(encoded_msg_proto_buf_header).to_bytes(4, byteorder='little', signed=False)
     await ws.send(header + encoded_msg_proto_buf_header + body)
+    return True
 
 async def handle_message(body):
-    if type(body) != 'bytes':
+    if not isinstance(body, bytes):
         return
     rawEmsg = int.from_bytes(body[0:4], byteorder='little')
     hdrLength = int.from_bytes(body[4:8], byteorder='little')
@@ -93,25 +109,27 @@ async def handle_message(body):
     msgBody = body[8+hdrLength:]
     if not rawEmsg & PROTO_MASK:
         raise ValueError(f"Received unexpected non-protobuf message {rawEmsg}")
-    
     msg_proto_buf_header = decode( steammessages__base__pb2.CMsgProtoBufHeader(), hdrBuf)
     print('msg_proto_buf_header: ', msg_proto_buf_header)
-    if msg_proto_buf_header['client_sessionid'] and msg_proto_buf_header['client_sessionid'] != client_session_id:
-        print(f'Got new client session id {msg_proto_buf_header["client_sessionid"]}');
-        client_session_id = msg_proto_buf_header['client_sessionid']
+    global client_session_id 
+    if msg_proto_buf_header['clientSessionid'] and msg_proto_buf_header['clientSessionid'] != client_session_id:
+        print(f'Got new client session id {msg_proto_buf_header["clientSessionid"]}');
+        client_session_id = msg_proto_buf_header['clientSessionid']
     eMsg = (rawEmsg & ~PROTO_MASK)  
     if eMsg != Multi:
-        print(f'Receive: { eMsg} ({msg_proto_buf_header["target_job_name"]})')
-
-    if msg_proto_buf_header['jobid_target'] and jobs[msg_proto_buf_header['jobid_target']]:
+        print(f'Receive: { eMsg} ({msg_proto_buf_header.get("target_job_name")})')
+    global jobs
+    if msg_proto_buf_header.get('jobid_target') and jobs[msg_proto_buf_header.get('jobid_target')]:
         pass
     if eMsg == CLIENT_LOG_ON_RESPONSE: # 751时 换服务器
         log_on_response = decode( steammessages__clientserver__login__pb2.CMsgClientLogonResponse(), msgBody)
-        print(f'Received ClientLogOnResponse with result: {log_on_response["eresult"]}')
+        print(f'Received ClientLogOnResponse with result: {log_on_response.get("eresult")}')
+        return False
     elif eMsg == Multi:
         pass
     else:
         print(f'Received unexpected message: {eMsg}')
+    return True
 
 async def main():
     while True: # 换服务器
@@ -125,7 +143,7 @@ async def main():
                         'protocol_version': PROTOCOL_VERSION
                     }
                     encoded_msg_client_hello = encode( steammessages__clientserver__login__pb2.CMsgClientHello(), msg_client_hello)
-                    await send_message(websocket, CLIENT_HELLO, encoded_msg_client_hello)
+                    send_result = await send_message(websocket, CLIENT_HELLO, encoded_msg_client_hello)
                     asyncio.create_task(ping(websocket))
                     timeout = 1000
                     while True: 
@@ -142,7 +160,9 @@ async def main():
                             print(f'Connecting to {cm["endpoint"]} timed out after {timeout} ms')
                             timeout = min(1000, timeout * 2)
                         else:
-                            await handle_message(response)
+                            handle_result = await handle_message(response)
+                            if not handle_result:
+                                break
                             return # 结束方法
                 break
             except ConnectionRefusedError as e:
