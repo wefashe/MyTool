1a06394d994e1d9ed5f5869f6fd8a7f77a9d4e11
wefashe <wefashe@qq.com>
2024-05-30 14:18:01 +0800

    优化请求头·

--- extract/steam_server.py
+++ extract/steam_server.py
@@ -17,20 +17,9 @@ def get_cm_server(): # 随机获取服务器信息
         # https://api.steampowered.com/ISteamDirectory/GetCMList/v1/?cellid=148&steamrealm=steamchina
         url = 'https://api.steampowered.com/ISteamDirectory/GetCMListForConnect/v0001/?cellid=0&format=js'
         headers = {
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'zh-CN,zh;q=0.9',
-            'Cache-Control': 'no-cache',
-            'Pragma': 'no-cache',
-            'Sec-Ch-Ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
-            'Sec-Ch-Ua-Mobile': '?0',
-            'Sec-Ch-Ua-Platform': '"Windows"',
-            'Sec-Fetch-Dest': 'document',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Site': 'none',
-            'Sec-Fetch-User': '?1',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
+            'user-agent': 'Valve/Steam HTTP Client 1.0',
+			'accept-charset': 'ISO-8859-1,utf-8,*;q=0.7',
+			'accept': 'text/html,*/*;q=0.9'
         }
         try:
             resp = requests.get(url, headers=headers)
