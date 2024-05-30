f65252e008dc5952ec6df0bc71ceb4bd9aa52f79
wefashe <wefashe@qq.com>
2024-05-30 11:34:00 +0800

    优化

--- extract/steam_cm.py
+++ extract/steam_cm.py
@@ -1,7 +1,6 @@
 
 import os
 import json
-import time
 import base64
 import asyncio
 import websockets
@@ -19,22 +18,20 @@ CLIENT_LOG_ON_RESPONSE = 751
 
 client_sessionid = 0
 
-async def send_message(ws, type):
+async def send_message(ws, send_type):
 
     global client_sessionid
     proto_header = steammessages_base_pb2.CMsgProtoBufHeader()
     proto_header.steamid = 0
     proto_header.client_sessionid = client_sessionid
-    proto_header_encoded = proto_header.SerializeToString() 
-
-    if type == CLIENT_HELLO:
+    if send_type == CLIENT_HELLO:
 
         proto_data = steammessages_clientserver_login_pb2.CMsgClientHello()
         proto_data.protocol_version = PROTOCOL_VERSION
-        proto_data_encoded = proto_data.SerializeToString() 
+        proto_data_buffer = proto_data.SerializeToString() 
         
 
-    if type == SERVICE_METHOD_CALL_FROM_CLIENT_NON_AUTHED:
+    if send_type == SERVICE_METHOD_CALL_FROM_CLIENT_NON_AUTHED:
         refresh_token = 'eyAidHlwIjogIkpXVCIsICJhbGciOiAiRWREU0EiIH0.eyAiaXNzIjogInI6MEVGMl8yNDM2MjkyOV9CNEFGRiIsICJzdWIiOiAiNzY1NjExOTkwMTIzODIwNzQiLCAiYXVkIjogWyAiY2xpZW50IiwgIndlYiIgXSwgImV4cCI6IDE3MTYwMTA1MTYsICJuYmYiOiAxNzA3MjgzNDMzLCAiaWF0IjogMTcxNTkyMzQzMywgImp0aSI6ICIxODA1XzI0NkVERDhBXzg5NTVBIiwgIm9hdCI6IDE3MTI0OTg3ODAsICJydF9leHAiOiAxNzMwNjIyNTM2LCAicGVyIjogMCwgImlwX3N1YmplY3QiOiAiMTg5LjEwNy4yMS41OSIsICJpcF9jb25maXJtZXIiOiAiMTg5LjEwNy4yMS41OSIgfQ.toPmNGeTtidvFbKPdGb2RDwSnBxZKCKJGny5QO0Za7OszFhR_tbWN4x6iq6_w9dOjoVty5R0HqN7IlEaDFDBAA'
         parts = refresh_token.split('.')
         if len(parts) != 3:
@@ -45,10 +42,11 @@ async def send_message(ws, type):
         proto_data = steammessages_auth_pb2.CAuthentication_AccessToken_GenerateForApp_Request()
         proto_data.refresh_token = refresh_token
         proto_data.steamid = int(data['sub'])
-        proto_data_encoded = proto_data.SerializeToString() 
+        proto_data_buffer = proto_data.SerializeToString() 
 
         targetName = 'Authentication.GenerateAccessTokenForApp#1';
 
+        # 生成包含 8 个随机字节的缓冲区
         job_id_buffer = bytearray(os.urandom(8))
         job_id_buffer[0] &= 0x7f  
         job_id = int.from_bytes(job_id_buffer, 'big')
@@ -57,55 +55,52 @@ async def send_message(ws, type):
         proto_header.target_job_name = targetName
         proto_header.realm = 1
 
-    header_encoded = bytearray(8)
-    header_encoded[:4] = (type | PROTO_MASK).to_bytes(4, byteorder='little', signed=False)
-    header_encoded[4:] = len(proto_header_encoded).to_bytes(4, byteorder='little', signed=False)
-    await ws.send(header_encoded + proto_header_encoded + proto_data_encoded)
+    proto_header_buffer = proto_header.SerializeToString() 
+    # 创建一个包含 8 个字节的缓冲区，并将所有字节的值初始化为 0
+    header_buffer = bytearray(8)
+    header_buffer[0:4] = (send_type | PROTO_MASK).to_bytes(4, byteorder='little', signed=False)
+    header_buffer[4:8] = len(proto_header_buffer).to_bytes(4, byteorder='little', signed=False)
+    await ws.send(header_buffer + proto_header_buffer + proto_data_buffer)
 
-async def rece_message(ws, type):
+async def recv_message(ws, recv_type):
     message = await ws.recv()
     if not isinstance(message, bytes):
         return
-    rawEmsg = int.from_bytes(message[0:4], byteorder='little')
-    hdrLength = int.from_bytes(message[4:8], byteorder='little')
-    hdrBuf = message[8:8+hdrLength]
-    msgBody = message[8+hdrLength:]
-    if not rawEmsg & PROTO_MASK:
-        raise ValueError(f"Received unexpected non-protobuf message {rawEmsg}")
+    # 读取字节数组前4位
+    msg_type = int.from_bytes(message[0:4], byteorder='little', signed=False) & (~PROTO_MASK)
+    # 读取字节数组4到8位
+    header_length = int.from_bytes(message[4:8], byteorder='little', signed=False)
+    # 数组8位到8+hdrLength位
+    header_buffer = message[8: 8+header_length]
+    # 数组8+hdrLength位之后
+    proto_data_buffer = message[8 + header_length:]
     
+    print('msg_type: ', msg_type)
     proto_header = steammessages_base_pb2.CMsgProtoBufHeader()
-    proto_header.ParseFromString(hdrBuf) 
+    proto_header.ParseFromString(header_buffer) 
     print(proto_header)
 
     global client_sessionid
     if proto_header.client_sessionid != client_sessionid:
         client_sessionid = proto_header.client_sessionid
 
-    eMsg = (rawEmsg & ~PROTO_MASK)  
-    if eMsg != Multi:
-        print(f'Receive: { eMsg} ({proto_header.target_job_name})')
-    global jobs
-    if proto_header.jobid_target:
-        pass
-    if eMsg == CLIENT_LOG_ON_RESPONSE: # 751时 换服务器
-        response = steammessages_clientserver_login_pb2.CMsgClientLogonResponse()
-        response.ParseFromString(msgBody) 
-        print(response)
-        print(f'Received ClientLogOnResponse with result: {response.eresult}')
-        return False
-    elif eMsg == Multi:
-        pass
-    else:
-        print(f'Received unexpected message: {eMsg}')
-        # steammessages_auth_pb2.CAuthentication_AccessToken_GenerateForApp_Response()
+    proto_data = steammessages_auth_pb2.CAuthentication_AccessToken_GenerateForApp_Response()
+    proto_data.ParseFromString(proto_data_buffer) 
+    print(proto_data)
+    
+    response = steammessages_clientserver_login_pb2.CMsgClientLogonResponse()
+    response.ParseFromString(proto_data_buffer) 
+    print(response)
+
+    
 
 async def ws():
-    async with websockets.connect(f'wss://ext2-hkg1.steamserver.net:27020/cmsocket/') as websocket:
+    async with websockets.connect(f'wss://ext6-hkg1.steamserver.net:27037/cmsocket/') as websocket:
         await send_message(websocket, CLIENT_HELLO)  
-        await rece_message(websocket, CLIENT_HELLO)
+        await recv_message(websocket, CLIENT_HELLO)
          
         await send_message(websocket, SERVICE_METHOD_CALL_FROM_CLIENT_NON_AUTHED) 
-        await rece_message(websocket, SERVICE_METHOD_CALL_FROM_CLIENT_NON_AUTHED) 
+        await recv_message(websocket, SERVICE_METHOD_CALL_FROM_CLIENT_NON_AUTHED) 
 
 
 
