bed4318211eb900e6d7d275d7e556649b459bbcb
wefashe <wefashe@qq.com>
2024-06-11 18:03:08 +0800

    完善

--- kami/client.py
+++ kami/client.py
@@ -6,6 +6,7 @@ import base64
 from datetime import datetime, timedelta
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
+from tkinter import messagebox
 
 import tkinter as tk
 from tkinter import ttk
@@ -56,6 +57,7 @@ def copy_to_clipboard(event):
     # 复制机器码到剪贴板
     event.widget.clipboard_clear()
     event.widget.clipboard_append(event.widget.get())
+    status_label.config(text="机器码复制成功！", fg="green")
 
 def start_countdown(count):
     total_seconds = count
@@ -68,35 +70,39 @@ def start_countdown(count):
     minutes = total_seconds // 60
 
     seconds = total_seconds % 60
-
     # 更新标签文本
-    label.config(text=f'剩余时间：{days}天{hours:02}小时{minutes:02}分钟{seconds:02}秒')
-    
+    win.title('卡密测试 ' + str(tk.TkVersion)+f'    剩余时间：{days}天{hours:02}小时{minutes:02}分钟{seconds:02}秒')
     if count > 0:
         # 每1000毫秒（1秒）更新一次
         win.after(1000, start_countdown, count - 1)
     else:
         # 倒计时结束时显示提示
-        label.config(text="已到期！")
+        win.title('卡密测试 ' + str(tk.TkVersion)+f'    剩余时间：已到期！')
+        status_label.config(text="卡密已到期", fg="red")
 
 def login():
     msg_text = card_code_entry.get()
     BLOCK_SIZE = AES.block_size # 16的倍数
     # key长度必须是16, 24, 32
     key = '9876543219876543'
-
-    encrypted_msg_bytes = bytes.fromhex(msg_text)
-    cipher = AES.new(key.encode('utf8'), AES.MODE_ECB)
-    cipher_decrypt = cipher.decrypt(encrypted_msg_bytes)
-    decrypted_msg = unpad(cipher_decrypt, BLOCK_SIZE)
-
-    print(decrypted_msg.decode('utf-8'))
-
-    license_str = base64.b64decode(decrypted_msg)
-    license_dict = eval(license_str)
+    try:
+        encrypted_msg_bytes = bytes.fromhex(msg_text)
+        cipher = AES.new(key.encode('utf8'), AES.MODE_ECB)
+        cipher_decrypt = cipher.decrypt(encrypted_msg_bytes)
+        decrypted_msg = unpad(cipher_decrypt, BLOCK_SIZE)
+        license_str = base64.b64decode(decrypted_msg)
+        license_dict = eval(license_str)
+    except Exception as e:
+        print(e)
+        status_label.config(text="卡密不正确", fg="red")
+        return
     print(license_dict)
 
     if check_exp_time(license_dict['exp']):
+        status_label.config(text="卡密已过期", fg="red")
+        return
+    if license_dict['psw'] != input_kami.get():
+        status_label.config(text="机器码不正确", fg="red")
         return
 
     now = get_beijin_time()
@@ -105,15 +111,23 @@ def login():
     date = exp - now
     seconds = date.seconds +  date.days * 24 * 60 * 60
     start_countdown(seconds)
+    status_label.config(text="登录成功", fg="green")
 
-        
+def check_input(*args):
+    # 获取输入框的值并去除两端的空格
+    input_value = entry_var.get().strip()
+    # 如果输入框的值非空且非空格，则启用按钮，否则禁用按钮
+    if input_value:
+        login_button.config(state=tk.NORMAL)
+    else:
+        login_button.config(state=tk.DISABLED)       
 
 win = tk.Tk()
-win.geometry("600x250")
+# win.geometry("600x250")
 win.title('卡密测试 ' + str(tk.TkVersion))
 win.resizable(False, False)
 show_kami = ttk.Label(win, text='机器码: ')
-show_kami.pack()
+show_kami.grid(row=0, column=0, padx=5, pady=5)
 
 
 input_kami = ttk.Entry(win, width=34)
@@ -121,25 +135,27 @@ mac_address = get_mac_address()
 machine_code = hash_msg(mac_address)
 input_kami.insert(0, machine_code)
 input_kami.config(state='readonly')
-input_kami.pack()
+input_kami.grid(row=0, column=1, padx=5, pady=5)
 # 绑定双击事件到复制功能
 input_kami.bind("<Double-1>", copy_to_clipboard)
 
 
+entry_var = tk.StringVar()
+entry_var.trace_add('write', check_input)
 # 创建并设置卡密输入框
-card_code_label = tk.Label(win, text="卡 密:")
-card_code_label.pack()
+card_code_label = tk.Label(win, text="卡 密: ")
+card_code_label.grid(row=1, column=0, padx=5, pady=10)
 
-card_code_entry = tk.Entry(win, width=35)
-card_code_entry.pack()
-
-# 创建标签用于显示倒计时
-label = tk.Label(win, width=35)
-label.pack(pady=20)
+card_code_entry = tk.Entry(win, width=34, textvariable=entry_var)
+card_code_entry.grid(row=1, column=1, padx=10, pady=10)
 
 # 创建登录按钮
-login_button = ttk.Button(win, text="登录", command=login)
-login_button.pack()
+login_button = ttk.Button(win, text="登录", command=login,width=30, state=tk.DISABLED)
+login_button.grid(row=2, columnspan=2, padx=5, pady=5)
+
+# 创建状态标签
+status_label = tk.Label(win)
+status_label.grid(row=3, columnspan=2, padx=5, pady=10)
 
 win.mainloop()
     
