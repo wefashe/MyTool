af4a7548c82db43e8ef7895eeecd0283421e5374
wefashe <wefashe@qq.com>
2024-06-13 19:52:33 +0800

    客户端完成

--- kami/client/control.py
+++ kami/client/control.py
@@ -5,10 +5,16 @@
 窗口控制, 业务逻辑
 """
 import uuid
+import base64
+import ntplib
 import hashlib
+import traceback
 import tkinter as tk
 from wingui import WinGUI
 from tkinter.messagebox import showinfo
+from datetime import datetime
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
 
 class Control:
 
@@ -27,7 +33,6 @@ class Control:
         # self.win.tk_input_machine_code.insert(0, machine_code)
         self.win.tk_var_machine_code.set(machine_code)
         self.win.tk_input_machine_code.config(state='readonly')
-        self.win.tk_input_register_code.config(state='readonly')
 
     def get_mac_address(self):
         mac = uuid.UUID(int=uuid.getnode()).hex[-12:]
@@ -41,7 +46,7 @@ class Control:
         res = sha256.hexdigest()
         return res.upper()[::2]
     
-    def check_button_register_code(self):
+    def check_button_register_code(self, *args):
         var_register_code = self.win.tk_var_register_code.get().strip()
         if var_register_code:
             self.win.tk_button_register_code.config(state=tk.NORMAL)
@@ -66,3 +71,75 @@ class Control:
         widget.delete(0, 'end')
         # widget.insert(0, self.win.clipboard_get())
         self.win.tk_var_register_code.set(self.win.clipboard_get())
+
+    def get_beijin_time(self):
+        try:
+            response = ntplib.NTPClient().request('ntp.aliyun.com')
+            return datetime.fromtimestamp(response.tx_time)
+        except Exception as e:
+            traceback.print_exc(file = open('error.log', 'a+'))
+            return datetime.now()
+
+    def check_exp_time(self, exp_time):
+        current_datetime = self.get_beijin_time()
+        exp_datetime = datetime.fromtimestamp(exp_time)
+        if current_datetime > exp_datetime:
+            return True
+        else:
+            return False
+        
+    def start_countdown(self, count):
+        total_seconds = count
+        days = total_seconds // (24 * 3600)
+        total_seconds = total_seconds % (24 * 3600)
+
+        hours = total_seconds // 3600
+        total_seconds %= 3600
+
+        minutes = total_seconds // 60
+
+        seconds = total_seconds % 60
+        # 更新标签文本
+        self.win.tk_label_expire_status.config(text = f'剩余使用时间：{days}天{hours:02}小时{minutes:02}分钟{seconds:02}秒')
+        if count > 0:
+            # 每1000毫秒（1秒）更新一次
+            self.win.after(1000, self.start_countdown, count - 1)
+        else:
+            # 倒计时结束时显示提示
+            self.win.tk_label_expire_status.config(text = f'剩余使用时间：已经到期')
+
+    def login(self, event):
+        var_register_code = self.win.tk_var_register_code.get().strip()
+        BLOCK_SIZE = AES.block_size # 16的倍数
+        # key长度必须是16, 24, 32
+        key = '9876543219876543'
+        try:
+            encrypted_msg_bytes = bytes.fromhex(var_register_code)
+            cipher = AES.new(key.encode('utf8'), AES.MODE_ECB)
+            cipher_decrypt = cipher.decrypt(encrypted_msg_bytes)
+            decrypted_msg = unpad(cipher_decrypt, BLOCK_SIZE)
+            license_str = base64.b64decode(decrypted_msg)
+            license_dict = eval(license_str)
+        except Exception as e:
+            traceback.print_exc(file = open('error.log', 'a+'))
+            showinfo(title="提示", message="注册码错误")
+            return
+        
+        psw = license_dict['psw']
+        if psw and psw != self.win.tk_var_machine_code.get().strip():
+            showinfo(title="提示", message="机器码不一致")
+            return
+        exp = license_dict['exp']
+        if exp:
+            if self.check_exp_time(exp):
+                showinfo(title="提示", message="注册码已过期")
+                return
+            now = self.get_beijin_time()
+            exp = datetime.fromtimestamp(license_dict['exp'])
+
+            date = exp - now
+            seconds = date.seconds +  date.days * 24 * 60 * 60
+            self.start_countdown(seconds)
+            showinfo(title="提示", message="注册成功")
+        else:
+            self.win.tk_label_expire_status.config(text = f'剩余使用时间：永久有效')
--- kami/client/wingui.py
+++ kami/client/wingui.py
@@ -14,8 +14,9 @@ class Win(Tk):
         self.__win()
         self.tk_input_machine_code,self.tk_var_machine_code,imc_x_pos,imc_y_pos = self.__tk_input_machine_code(self)
         self.tk_button_machine_code,bmc_x_pos,bmc_y_pos = self.__tk_button_machine_code(self,x_pos=imc_x_pos + 5)
-        self.tk_input_register_code,self.tk_var_register_code,irc_x_pos,irc_y_pos = self.__tk_input_register_code(self, y_pos = 70)
-        self.tk_button_register_code,brc_x_pos,brc_y_pos = self.__tk_button_register_code(self,x_pos=imc_x_pos + 5, y_pos = 70)
+        self.tk_input_register_code,self.tk_var_register_code,irc_x_pos,irc_y_pos = self.__tk_input_register_code(self, y_pos = 65)
+        self.tk_button_register_code,brc_x_pos,brc_y_pos = self.__tk_button_register_code(self,x_pos=irc_x_pos + 5, y_pos = 65)
+        self.tk_label_expire_status,les_x_pos,les_y_pos = self.__tk_label_expire_status(self, y_pos = 105)
 
     def __win(self):
         self.title("被注册软件")
@@ -57,11 +58,18 @@ class Win(Tk):
         return entry, stringVar, x_pos + label_width + entry_width, y_pos+height
 
     def __tk_button_register_code(self, parent, x_pos = 20, y_pos = 20):
-        button = Button(parent, text="粘贴", takefocus=False,)
+        button = Button(parent, text="注册", takefocus=False, state=DISABLED)
         button_width = 50
         button_height = 30
         button.place(x=x_pos, y=y_pos, width=button_width, height=button_height)
         return button, x_pos+button_width, y_pos+button_height
+    
+    def __tk_label_expire_status(self, parent, x_pos = 20, y_pos = 20):
+        label = Label(parent, )
+        label_width = 350
+        label_height = 30
+        label.place(x=x_pos, y=y_pos, width=label_width, height=label_height)
+        return label, x_pos + label_width + label_width, y_pos+label_height
 
     def show(self):
         self.mainloop()
@@ -78,8 +86,9 @@ class WinGUI(Win):
         """
         事件绑定
         """
+        self.tk_var_register_code.trace_add('write', self.ctl.check_button_register_code)
         self.tk_button_machine_code.bind('<Button-1>',lambda event: self.ctl.copy_to_clipboard(event, self.tk_input_machine_code))
-        self.tk_button_register_code.bind('<Button-1>',lambda event: self.ctl.paste_from_clipboard(event, self.tk_input_register_code))
+        self.tk_button_register_code.bind('<Button-1>', self.ctl.login)
 
 if __name__ == "__main__":
     win = Win()
