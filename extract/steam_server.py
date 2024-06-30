cf2e3cf0980f81ebbac8e0f9608c3ebd0388b124
wefashe <wefashe@qq.com>
2024-06-30 23:16:27 +0800

    完善

--- extract/steam_server.py
+++ extract/steam_server.py
@@ -35,9 +35,6 @@ def get_cm_list():
     return cmList
 
 cmList = get_cm_list()
-cm = cmList[random.randint(0, min(20, len(cmList)) - 1)]
-endpoint = cm["endpoint"]
-logging.debug(f"Received message: {endpoint}")
 
 PROTO_MASK = 0x80000000
 jobs = []
@@ -106,51 +103,51 @@ def decode_jwt(jwt: str) -> dict:
     decoded_str = decoded_bytes.decode('utf-8')
     return json.loads(decoded_str)
 
+
 async def connect_to_cm():
-    connect_timeout = 5
-    try:
-        async with websockets.connect(f'wss://{endpoint}/cmsocket/') as ws:
+    while True:
+        cm = cmList[random.randint(0, min(20, len(cmList)) - 1)]
+        endpoint = cm["endpoint"]
+        logging.debug(f"Received message: {endpoint}")
+        try:
+            async with websockets.connect(f'wss://{endpoint}/cmsocket/') as ws:
 
-            logging.debug(f"Connected to {endpoint}")
+                logging.debug(f"Connected to {endpoint}")
 
-            PROTOCOL_VERSION = 65580
-            client_hello_builder = steammessages_clientserver_login_pb2.CMsgClientHello()
-            client_hello_builder.protocol_version = PROTOCOL_VERSION
-            client_hello_body = client_hello_builder.SerializeToString()
+                PROTOCOL_VERSION = 65580
+                client_hello_builder = steammessages_clientserver_login_pb2.CMsgClientHello()
+                client_hello_builder.protocol_version = PROTOCOL_VERSION
+                client_hello_body = client_hello_builder.SerializeToString()
 
-            CLIENT_HELLO = 9805
-            await send_message(ws, CLIENT_HELLO, client_hello_body)
+                CLIENT_HELLO = 9805
+                await send_message(ws, CLIENT_HELLO, client_hello_body)
 
-            while True:
-                refreshToken = input("\nrefresh_token: ")
-                if refreshToken.lower() == 'exit':
-                    break
-                # refreshToken = 'eyAidHlwIjogIkpXVCIsICJhbGciOiAiRWREU0EiIH0.eyAiaXNzIjogInN0ZWFtIiwgInN1YiI6ICI3NjU2MTE5OTE3NjgzMjExOCIsICJhdWQiOiBbICJjbGllbnQiLCAid2ViIiwgInJlbmV3IiwgImRlcml2ZSIgXSwgImV4cCI6IDE3MjgxNzkxODUsICJuYmYiOiAxNjg4NTk1MzA0LCAiaWF0IjogMTY5NzIzNTMwNCwgImp0aSI6ICIxNDU1XzIzNTBDMUJBX0UwQTA5IiwgIm9hdCI6IDE2OTcyMzUzMDQsICJnZW4iOiAxLCAicGVyIjogMSwgImlwX3N1YmplY3QiOiAiODIuMTEuMTU0LjUwIiwgImlwX2NvbmZpcm1lciI6ICI4Mi4xMS4xNTQuNTAiIH0.GDKSalpYq1c3f9NdPHqwxj3-QY_Jgx8by6GCAy1ftGraOK91b4TdQx9PGADTIc0U00K5JX3-GLsShO5xgXepDw'
-                accesstoken_request_builder = steammessages_auth_pb2.CAuthentication_AccessToken_GenerateForApp_Request()
-                accesstoken_request_builder.refresh_token = refreshToken
-                accesstoken_request_builder.steamid = int(decode_jwt(refreshToken)['sub'])
-                accesstoken_request_body = accesstoken_request_builder.SerializeToString()
-                ServiceMethodCallFromClientNonAuthed = 9804
-                await send_message(ws, ServiceMethodCallFromClientNonAuthed, accesstoken_request_body)
-                try:
-                    while True:
-                        body = await ws.recv()
-                        if not isinstance(body, bytes):
-                            continue
-                        handle_message(body)
+                while True:
+                    refreshToken = input("\nrefresh_token: ")
+                    if refreshToken.lower() == 'exit':
                         break
-                except websockets.exceptions.ConnectionClosedError:
-                    print("连接关闭，停止接收消息")
-                except Exception as e:
-                    print(f"接收消息时发生错误: {e}")        
-    except asyncio.TimeoutError:
-        logging.debug(f"Connecting to {endpoint} timed out after {connect_timeout} ms")
-        connect_timeout = min(10000, connect_timeout * 2)
-        await asyncio.sleep(1)  # 等待一段时间再重试
-    except websockets.ConnectionClosed as e:
-        logging.debug(f"Disconnected from {endpoint}: {e.code} ({e.reason})")
-        await asyncio.sleep(1)
-    except Exception as e:
-        logging.debug(f"Error in WebSocket with {endpoint}: {e}")
+                    # refreshToken = 'eyAidHlwIjogIkpXVCIsICJhbGciOiAiRWREU0EiIH0.eyAiaXNzIjogInN0ZWFtIiwgInN1YiI6ICI3NjU2MTE5OTE3NjgzMjExOCIsICJhdWQiOiBbICJjbGllbnQiLCAid2ViIiwgInJlbmV3IiwgImRlcml2ZSIgXSwgImV4cCI6IDE3MjgxNzkxODUsICJuYmYiOiAxNjg4NTk1MzA0LCAiaWF0IjogMTY5NzIzNTMwNCwgImp0aSI6ICIxNDU1XzIzNTBDMUJBX0UwQTA5IiwgIm9hdCI6IDE2OTcyMzUzMDQsICJnZW4iOiAxLCAicGVyIjogMSwgImlwX3N1YmplY3QiOiAiODIuMTEuMTU0LjUwIiwgImlwX2NvbmZpcm1lciI6ICI4Mi4xMS4xNTQuNTAiIH0.GDKSalpYq1c3f9NdPHqwxj3-QY_Jgx8by6GCAy1ftGraOK91b4TdQx9PGADTIc0U00K5JX3-GLsShO5xgXepDw'
+                    accesstoken_request_builder = steammessages_auth_pb2.CAuthentication_AccessToken_GenerateForApp_Request()
+                    accesstoken_request_builder.refresh_token = refreshToken
+                    accesstoken_request_builder.steamid = int(decode_jwt(refreshToken)['sub'])
+                    accesstoken_request_body = accesstoken_request_builder.SerializeToString()
+                    ServiceMethodCallFromClientNonAuthed = 9804
+                    await send_message(ws, ServiceMethodCallFromClientNonAuthed, accesstoken_request_body)
+                    try:
+                        while True:
+                            body = await ws.recv()
+                            if not isinstance(body, bytes):
+                                continue
+                            handle_message(body)
+                            break
+                    except websockets.exceptions.ConnectionClosedError as e:
+                        logging.debug(f"recv Disconnected from {endpoint}: {e.code} ({e.reason})")
+                    except Exception as e:
+                        logging.debug(f"recv Error in WebSocket with {endpoint}: {e}")   
+                    refreshToken = None  
+        except websockets.ConnectionClosed as e:
+            logging.debug(f"Disconnected from {endpoint}: {e.code} ({e.reason})")
+        except Exception as e:
+            logging.debug(f"Error in WebSocket with {endpoint}: {e}")
 
 asyncio.run(connect_to_cm())
