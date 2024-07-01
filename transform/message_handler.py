9bb4573f364eb6f877fc2139c37aa3de003cb9f1
wefashe <wefashe@qq.com>
2024-07-02 01:53:10 +0800

    完善

--- transform/message_handler.py
+++ transform/message_handler.py
@@ -1,8 +1,5 @@
 import os
 import gzip
-import asyncio
-import logging
-import websockets
 
 from emsg_enum import EMsg
 from eresult_enum import EResult
@@ -13,40 +10,32 @@ import steammessages_clientserver_login_pb2
 import steammessages_base_pb2
 import steammessages_auth_pb2
 
-logging.basicConfig(level=logging.DEBUG)
-
 PROTO_MASK = 0x80000000
 jobs = []
 
 async def send_message(ws, emsg:EMsg, body):
-    try:
-        proto_buf_header_builder = steammessages_base_pb2.CMsgProtoBufHeader()
-        proto_buf_header_builder.steamid = 0
-        proto_buf_header_builder.client_sessionid = 0
-        if emsg == EMsg.ServiceMethodCallFromClientNonAuthed:
-            jobIdBuffer = bytearray(os.urandom(8))
-            jobIdBuffer[0] &= 0x7F
-            jobId = int.from_bytes(jobIdBuffer, byteorder='big', signed=False)
-            jobs.append(jobId)
-            proto_buf_header_builder.jobid_source = jobId
-            proto_buf_header_builder.target_job_name = 'Authentication.GenerateAccessTokenForApp#1'
-            proto_buf_header_builder.realm = 1
-        proto_buf_header_body = proto_buf_header_builder.SerializeToString()
-        header = bytearray(8)
-        header[:4] = (emsg.value | PROTO_MASK).to_bytes(4, byteorder='little', signed=False)
-        header[4:] = len(proto_buf_header_body).to_bytes(4, byteorder='little', signed=False)
-        await ws.send(header + proto_buf_header_body + body)
-    except websockets.ConnectionClosed as e:
-        logging.ERROR(f"连接关闭，状态码: {e.code}, 原因: {e.reason}")
-    except Exception as e:
-        logging.ERROR(f"发送错误: {e}")
+    proto_buf_header_builder = steammessages_base_pb2.CMsgProtoBufHeader()
+    proto_buf_header_builder.steamid = 0
+    proto_buf_header_builder.client_sessionid = 0
+    if emsg == EMsg.ServiceMethodCallFromClientNonAuthed:
+        jobIdBuffer = bytearray(os.urandom(8))
+        jobIdBuffer[0] &= 0x7F
+        jobId = int.from_bytes(jobIdBuffer, byteorder='big', signed=False)
+        jobs.append(jobId)
+        proto_buf_header_builder.jobid_source = jobId
+        proto_buf_header_builder.target_job_name = 'Authentication.GenerateAccessTokenForApp#1'
+        proto_buf_header_builder.realm = 1
+    proto_buf_header_body = proto_buf_header_builder.SerializeToString()
+    header = bytearray(8)
+    header[:4] = (emsg.value | PROTO_MASK).to_bytes(4, byteorder='little', signed=False)
+    header[4:] = len(proto_buf_header_body).to_bytes(4, byteorder='little', signed=False)
+    await ws.send(header + proto_buf_header_body + body)
 
 async def send_hello(ws):
     PROTOCOL_VERSION = 65580
     client_hello_builder = steammessages_clientserver_login_pb2.CMsgClientHello()
     client_hello_builder.protocol_version = PROTOCOL_VERSION
     client_hello_body = client_hello_builder.SerializeToString()
-
     await send_message(ws, EMsg.ClientHello, client_hello_body)
 
 async def send_token(ws, token):
@@ -55,21 +44,6 @@ async def send_token(ws, token):
     accesstoken_request_builder.steamid = int(token_decode.get_token_info(token)['sub'])
     accesstoken_request_body = accesstoken_request_builder.SerializeToString()
     await send_message(ws, EMsg.ServiceMethodCallFromClientNonAuthed, accesstoken_request_body)
-
-async def read_file_lines(file_path):
-    with open(file_path, 'r') as file:
-        for line in file:
-            yield line.strip()[7:]
-
-async def handle_file(ws, file_path):
-    tasks = []
-    async for line in read_file_lines(file_path):
-        obj = token_decode.get_token_info(line)
-        print(obj['exp'])
-        task = asyncio.create_task(send_token(ws, line))
-        tasks.append(task)
-        await asyncio.sleep(0.1)  # 控制发送速度
-    await asyncio.gather(*tasks)  # 确保所有任务完成
 
 
 def process_multi_msg(body):
@@ -80,8 +54,9 @@ def process_multi_msg(body):
         payload = gzip.decompress(payload)
     while len(payload) > 0:
         chunkSize = int.from_bytes(payload[0:4], byteorder='little')
-        handle_message(payload[4:4+chunkSize])
+        resp_handle = handle_message(payload[4:4+chunkSize])
         payload = payload[4+chunkSize:]
+    return  resp_handle
 
 def handle_message(message):
     rawEmsg = int.from_bytes(message[0:4], byteorder='little')
@@ -95,16 +70,17 @@ def handle_message(message):
     eMsg:EMsg = EMsg(rawEmsg & ~PROTO_MASK)
     if proto_buf_header.jobid_target in jobs:
         jobs.remove(proto_buf_header.jobid_target) 
-        result:EResult = EResult(proto_buf_header.eresult)
+        result:EResult = EResult(proto_buf_header.eresult) 
         if result == EResult.OK:
             accesstoken_response = steammessages_auth_pb2.CAuthentication_AccessToken_GenerateForApp_Response()
             accesstoken_response.ParseFromString(msgBody)
-            print('access_token:', accesstoken_response.access_token)
-            print('cookie:','steamLoginSecure='+ token_decode.get_cookie(accesstoken_response.access_token))
+            access_token = accesstoken_response.access_token
+            # print('access_token:', accesstoken_response.access_token)
+            return access_token
+            # print('cookie:','steamLoginSecure='+ token_decode.get_cookie(accesstoken_response.access_token))
         else:
             error_message = proto_buf_header.error_message
-            print(result, error_message)
-        return
+            return None
     if eMsg == EMsg.ClientLogOnResponse:
         logon_response = steammessages_clientserver_login_pb2.CMsgClientLogonResponse()
         result:EResult = EResult(logon_response.eresult)
@@ -112,23 +88,13 @@ def handle_message(message):
             pass
         elif result == EResult.TryAnotherCM or result == EResult.ServiceUnavailable:
             print(result)
+        return None
     elif eMsg == EMsg.Multi:
-        process_multi_msg(msgBody)
+        return process_multi_msg(msgBody)
     else:
         print(f'Received unexpected message: {eMsg}')
+        return None
         
 async def recv_message(ws):
-    try:
-        while True:
-            message = await ws.recv()
-            if not isinstance(message, bytes):
-                continue
-            handle_message(message)
-    except websockets.ConnectionClosed as e:
-        logging.ERROR(f"连接关闭，状态码: {e.code}, 原因: {e.reason}")
-    except Exception as e:
-        logging.ERROR(f"接收错误: {e}")
-
-async def send_heart(ws, interval):
-    await ws.send("PING")
-    await asyncio.sleep(interval)
+    message = await ws.recv()
+    return handle_message(message)
--- transform/token_transform.py
+++ transform/token_transform.py
@@ -1,5 +1,5 @@
 
-
+import time
 import random
 import asyncio
 import logging
@@ -9,40 +9,94 @@ import websockets
 import steam_api
 import message_handler
 
+from queue import Queue
+from concurrent.futures import ThreadPoolExecutor,as_completed
 
-logging.basicConfig(level=logging.DEBUG)
-
-cmList = steam_api.get_cm_list()
-if not cmList or len(cmList) == 0:
-    raise ValueError('未获取到服务器列表')
+logging.basicConfig(level=logging.ERROR)
 
 
-async def connect_to_cm():
-    while True:
-        try:
-            endpoint = cmList[random.randint(0, min(20, len(cmList)) - 1)]["endpoint"]
-            logging.info(f"开始连接 {endpoint} ")
-            async with websockets.connect(f'wss://{endpoint}/cmsocket/') as ws:
-                logging.info(f"成功连接 {endpoint} ")
+async def connect_cm(cmList, batch, success_queue, failure_queue):
+    endpoint = cmList[random.randint(0, min(20, len(cmList)) - 1)]["endpoint"]
+    logging.info(f"开始连接 {endpoint} ")
+    async with websockets.connect(f'wss://{endpoint}/cmsocket/') as ws:
+        logging.info(f"成功连接 {endpoint} ")
 
-                logging.info("开始发送认证包")
-                await message_handler.send_hello(ws)
-                logging.info("成功发送认证包")
+        logging.info("开始发送认证包")
+        await message_handler.send_hello(ws)
+        logging.info("成功发送认证包")
+
+        key = 'eyAidHlwIjogIkpXVCIsICJhbGciOiAiRWREU0EiIH0'
+
+        for refresh in batch:
+            await message_handler.send_token(ws, refresh[refresh.find(key):])
+            accesss = await message_handler.recv_message(ws)
+            if accesss:
+                success_queue.put((refresh, refresh[0:refresh.find(key)] + accesss))           
+            else:
+                failure_queue.put(refresh)
+
+def start_websocket_connection(cmList, batch, success_queue, failure_queue):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(connect_cm(cmList, batch, success_queue, failure_queue))
 
 
-                recv_task = asyncio.create_task(message_handler.recv_message(ws))
+file_path = r'transform/test.txt'
 
-                while True:
-                    file_path = input("请输入文件路径: ")
-                    if file_path.lower() == 'exit':
-                        break
-                    await message_handler.handle_file(ws, file_path)
+def process_file_in_batches(file_path, batch_size):
+    with open(file_path, 'r') as file:
+        while True:
+            batch = [file.readline().strip() for _ in range(batch_size)]
+            # 过滤掉空行
+            batch = [line for line in batch if line]
+            if not batch:
+                break
+            yield batch
 
-                recv_task.cancel() 
 
-        except websockets.ConnectionClosed as e:
-            logging.ERROR(f"Disconnected from {endpoint}: {e.code} ({e.reason})")
-        except Exception as e:
-            logging.ERROR(f"Error in WebSocket with {endpoint}: {e}")
+def main():
 
-asyncio.run(connect_to_cm())
+    start_time_insert = time.time()
+
+    batch_size = 100  # 每批处理的行数
+    max_workers = 10  # 最大线程数
+
+    cmList = steam_api.get_cm_list()
+    if not cmList or len(cmList) == 0:
+        raise ValueError('未获取到服务器列表')
+
+    success_queue = Queue()
+    failure_queue = Queue()
+
+    completed_task_count = 0
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = []
+        for batch in process_file_in_batches(file_path, batch_size):
+            future = executor.submit(start_websocket_connection, cmList, batch, success_queue, failure_queue)
+            futures.append(future)
+        for future in as_completed(futures):
+            future.result()
+            completed_task_count += 1
+            print("已经完成 "+ str(completed_task_count))
+
+    success_accesss_file_path = "临时成功.txt"
+    success_refresh_file_path = "有效ey.txt"
+    failure_refresh_file_path = "失败ey.txt"
+
+    # 写入成功和失败的数据到文件
+    with open(success_accesss_file_path, 'w') as success_accesss_file, \
+        open(success_refresh_file_path, 'w') as success_refresh_file, \
+            open(failure_refresh_file_path, 'w') as failure_refresh_file:
+        while not success_queue.empty():
+            refresh, accesss = success_queue.get()
+            success_accesss_file.write(accesss+'\n')
+            success_refresh_file.write(refresh+'\n')
+        
+        while not failure_queue.empty():
+            refresh = failure_queue.get()
+            failure_refresh_file.write(refresh+'\n')
+
+    end_time_insert = time.time()
+    insert_time = end_time_insert - start_time_insert
+    print(f'转换完成, 耗时{insert_time:.6f}秒')
+main()
